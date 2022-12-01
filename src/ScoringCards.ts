7e1974fbf61f8d6d1755a4821507cb044cd52e96
drsmile1001 <drsmile.1001@gmail.com>
2022-12-01 08:45:57 +0000

    feat: 新增部分計分卡資料

--- src/ScoringCards.ts
+++ src/ScoringCards.ts
@@ -53,4 +53,12 @@ export const scoringCards: ScoringCard[] = [
     1
   ),
   new IndexPlusScoringCard("D-006", "設立冒險者學校", 0, "迷宮板塊", 2),
+  new IndexPlusScoringCard("E-002", "王國公主夏洛特", 1, "生活", 2),
+  new IndexPlusScoringCard("E-003", "騎士公主加娜玖", 1, "據點", 1),
+  new IndexPlusScoringCard("E-004", "球形關節少女", 0, "零件", 2),
+  new IndexPlusScoringCard("E-005", "人狼女王", 2, "力量", 3), //最多5分
+  new IndexPlusScoringCard("E-006", "聖女馬卡龍", 1, "深淵洞穴", 0.5),
+  new IndexPlusScoringCard("E-009", "傳說商人芙蕾茲", 1, "商業", 2),
+  new IndexPlusScoringCard("E-010", "勇者羅薩特", 1, "迷宮板塊", 2),
+  new IndexPlusScoringCard("E-011", "流浪魔女歐佩拉", 1, "藥水", 2), //最多5分
 ]
