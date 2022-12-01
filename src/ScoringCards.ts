b11a4430fd38d862ebe82f6e9c671a438a55bc30
drsmile1001 <drsmile.1001@gmail.com>
2022-12-01 12:54:35 +0000

    feat: 調整卡片翻譯

--- src/ScoringCards.ts
+++ src/ScoringCards.ts
@@ -45,24 +45,24 @@ export class IndexPlusScoringCard implements ScoringCard {
 }
 
 export const scoringCards: ScoringCard[] = [
-  new IndexPlusScoringCard("D-001", "異世界食堂", 1, "卡面上的冒險者", 2),
-  new IndexPlusScoringCard("D-002", "老舊投影機", 0, "桌面的事件卡", 3),
+  new IndexPlusScoringCard("D-001", "異世界拉麵食堂", 1, "卡面上的冒險者", 2),
+  new IndexPlusScoringCard("D-002", "老舊的放映機", 0, "桌面的事件卡", 3, 6),
   new IndexPlusScoringCard("D-003", "聖女親衛隊", 0, "深淵洞穴", 1),
   new IndexPlusScoringCard("D-004", "迷宮開拓村", 2, "卡面上的冒險者", 0.5),
   new IndexPlusScoringCard(
     "D-005",
-    "吟遊詩人的敘事歌",
+    "吟遊詩人的傳唱",
     0,
     "生活商業零件之中持有數量最少的標記",
     1
   ),
   new IndexPlusScoringCard("D-006", "設立冒險者學校", 0, "迷宮板塊", 2),
-  new IndexPlusScoringCard("E-002", "王國公主夏洛特", 1, "生活", 2),
-  new IndexPlusScoringCard("E-003", "騎士公主加娜玖", 1, "據點", 1),
+  new IndexPlusScoringCard("E-002", "夏爾洛特 皇女", 1, "生活", 2),
+  new IndexPlusScoringCard("E-003", "騎士公主 甘娜許", 1, "據點", 1),
   new IndexPlusScoringCard("E-004", "球形關節少女", 0, "零件", 2),
   new IndexPlusScoringCard("E-005", "人狼女王", 2, "力量", 3, 5),
   new IndexPlusScoringCard("E-006", "聖女馬卡龍", 1, "深淵洞穴", 0.5),
-  new IndexPlusScoringCard("E-009", "傳說商人芙蕾茲", 1, "商業", 2),
-  new IndexPlusScoringCard("E-010", "勇者羅薩特", 1, "迷宮板塊", 2),
-  new IndexPlusScoringCard("E-011", "流浪魔女歐佩拉", 1, "藥水", 2, 5),
+  new IndexPlusScoringCard("E-009", "傳奇旅行商人費蕾茲", 1, "商業", 2),
+  new IndexPlusScoringCard("E-010", "勇者蘿謝特", 1, "迷宮板塊", 2),
+  new IndexPlusScoringCard("E-011", "流浪魔女歐貝拉", 1, "藥水", 2, 5),
 ]
